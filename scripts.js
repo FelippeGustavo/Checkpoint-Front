2a1a4baf85e43fcfdf4ad8620b9a794cf8690ae1
FelippeGustavo <73455806+FelippeGustavo@users.noreply.github.com>
2022-03-21 22:51:45 -0300

    parte 3

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
-const btn = document.querySelector(".sub");
-btn.addEventListener("click", function(f){
+const form = document.querySelector('.sub');
 
-f.preventDefault();
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-const name=document.querySelectorAll("#forms")
-const inputs = document.createTextNode(forms.value);
-document.body.appendChild(inputs);
-console.log(inputs)
+    const nome = document.querySelector('#name');
+    const ano = document.querySelector('#year');
+    const sobre = document.querySelector('#inf');
+    const link = document.querySelector('#link');
 
-})
+    document.body.innerHTML += `<div>${nome.value}</div>`
+    document.body.innerHTML += `<div>${ano.value}</div>`
+    document.body.innerHTML += `<div>${sobre.value}</div>`
+    document.body.innerHTML += `<div>${link.value}</div>`
+    
+}); 
